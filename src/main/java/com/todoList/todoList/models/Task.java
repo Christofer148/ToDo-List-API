df19cef02ad4f08aae3838d1a067a44d670364dd
Christofer Alcaraz <christofer.alcaraz148@gmail.com>
2023-11-08 23:10:00 -0600

    Adding Task model

---
+++ src/main/java/com/todoList/todoList/models/Task.java
@@ -0,0 +1,31 @@
+package com.todoList.todoList.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_tasks")
+@Data
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	
+	private String description;
+	
+	private Boolean completed;
+	
+	@Temporal(TemporalType.DATE)
+	private Date deadLine;
+}
